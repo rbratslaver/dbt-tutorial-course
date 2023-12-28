1a52dc97231e2e62837406f3be9b847ade5bb527
Roman <romanb333@gmail.com>
2023-12-28 02:55:29 -0500

    added!

--- lessons/models/example/my_first_dbt_model.sql
+++ lessons/models/example/my_first_dbt_model.sql
@@ -1,7 +1,7 @@
 
 
 /*
-    Welcome to your first dbt model!
+    Welcome to your first dbt model!!
     Did you know that you can also configure models directly within SQL files?
     This will override configurations stated in dbt_project.yml
 
